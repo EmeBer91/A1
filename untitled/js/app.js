a59984658f85edbbca19e620d5650f6a11827837
EmeBer91 <emeber987@ihmeducation.se>
2024-09-20 12:59:57 +0200

    Ändringar

--- untitled/js/app.js
+++ untitled/js/app.js
@@ -6,12 +6,17 @@ let button0 = document.getElementById("button0");
 let playerRollText = document.getElementById("playerRollText");
 let computerRollText = document.getElementById("computerRollText");
 let gameResultText = document.getElementById("gameResultText");
+let playerWinsText = document.getElementById("playerWinsText");
+let computerWinsText = document.getElementById("computerWinsText");
+let drawsText = document.getElementById("drawsText");
 
 
 //Data
 let playerRoll = 0;
 let computerRoll = 0;
-
+let playerWins = 0;
+let computerWins = 0;
+let draws = 0;
 
 //Processes
 button0.addEventListener("click", function() {
@@ -19,6 +24,7 @@ getRandomNumberOneToSixForPlayer();
 showPlayerRollResult ();
 showComputerRollResult ();
 determineWinner();
+updateScoreBoard();
 
 });
 
@@ -30,20 +36,30 @@ function getRandomNumberOneToSixForPlayer(){
 
 //Views
 function showPlayerRollResult() {
-  playerRollText.innerText = playerRoll;
+  playerRollText.innerText = "Player Roll:" + playerRoll;
+
 }
 
 function showComputerRollResult() {
-  computerRollText.innerText = computerRoll;
+  computerRollText.innerText = "Computer Roll: " + computerRoll;
 }
 
 // Determine Winner
 function determineWinner() {
   if (playerRoll > computerRoll) {
     gameResultText.innerText = "Result: You win!";
+    playerWins++;
   } else if (playerRoll < computerRoll) {
     gameResultText.innerText = "Result: Computer wins!";
+    computerWins++;
   } else {
     gameResultText.innerText = "Result: It's a tie!";
+    draws++;
   }
+}
+// Update Scoreboard
+function updateScoreBoard() {
+  playerWinsText.innerText = "Player Wins: " + playerWins;
+  computerWinsText.innerText = "Computer Wins: " + computerWins;
+  drawsText.innerText = "Draws: " + draws;
 }
