c71781ecb025ea8880daedf5556bfac86dd7618e
EmeBer91 <emeber987@ihmeducation.se>
2024-09-30 13:26:03 +0200

    Övning

--- Lastassignment/js/app.js
+++ Lastassignment/js/app.js
@@ -1,12 +1,29 @@
 //A B C D F
-
-let grade = 0;
+const gradeIdTextField = document.getElementById("grade");
+let grade =90;
 
 function getGrade() {
-  if grade <=90 && grade <=100
-    return "A"
+  if (grade >=90 && grade <=100) {
+    gradeIdTextField.innerText = "A";
 }
 
 else if (grade >=80 && grade <=89) {
+  gradeIdTextField.innerHTML= "B";
+  }
 
-}
+  else if (grade >=70 && grade <=79) {
+    gradeIdTextField.innerHTML= "C";
+  }
+
+  else if (grade >=60 && grade <=69) {
+    gradeIdTextField.innerHTML= "D";
+  }
+
+  else if (grade >=0 && grade<=59) {
+    gradeIdTextField.innerHTML= "F";
+  }
+
+  }
+getGrade();
+
+
